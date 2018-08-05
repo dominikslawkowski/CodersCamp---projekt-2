e1b0485ffa5ceddb9486c1066757b4454bb2636e
JestemBartek <maciek.bartynski@gmail.com>
2018-08-05 13:32:31 +0200

    ostatni commit

--- sn4-script.js
+++ sn4-script.js
@@ -8,12 +8,12 @@ function initializeGallerySection() {
 }
 
 function initGalleryNavBtns() {
-    let btns = objectToArray(document.querySelectorAll('.sn5_nav_dot-btn'));
-    let wrapper = document.querySelector('.sn5_gallery-wrapper');
+    let btns = objectToArray(document.querySelectorAll('.sn4_nav_dot-btn'));
+    let wrapper = document.querySelector('.sn4_gallery-wrapper');
     btns.forEach(function (btn, idx) {
         btn.addEventListener('click', function () {
-            btns.forEach(btn => btn.classList.remove('sn5_clicked'));
-            btn.classList.add('sn5_clicked');
+            btns.forEach(btn => btn.classList.remove('sn4_clicked'));
+            btn.classList.add('sn4_clicked');
             wrapper.style.left = (idx * 100) * -1 + '%';
             initGelleryMobileNavBtns(wrapper, btns, idx);
         })
@@ -27,7 +27,7 @@ function initGelleryMobileNavBtns(wrapper, btns, idx) {
 }
 
 function initGalleryMobileNavBar() {
-    let bar = document.querySelector('.sn5_nav');
+    let bar = document.querySelector('.sn4_nav');
     let lastPosition = window.pageYOffset;
     window.addEventListener('scroll', function () {
         if (bar.parentNode.offsetWidth <= 620) {
@@ -43,31 +43,31 @@ function initGalleryMobileNavBar() {
 }
 
 function stickyGalleryMobileNavScrollingDown(newPosition) {
-    let gallery = document.querySelector('.sn5_gallery-wrapper');
+    let gallery = document.querySelector('.sn4_gallery-wrapper');
     let roof = gallery.offsetTop;
     let floor = roof + gallery.offsetHeight;
     let perimeter = (window.innerHeight / 3) * 2;
     if (newPosition > roof && (newPosition + perimeter) < floor) {
-        document.querySelector('.sn5_nav').classList.add('sn5_nav_fixed');
+        document.querySelector('.sn4_nav').classList.add('sn4_nav_fixed');
     } else {
-        document.querySelector('.sn5_nav').classList.remove('sn5_nav_fixed');
+        document.querySelector('.sn4_nav').classList.remove('sn4_nav_fixed');
     }
 }
 
 function stickyGalleryMobileNavScrollingUp(newPosition) {
-    let gallery = document.querySelector('.sn5_gallery-wrapper');
+    let gallery = document.querySelector('.sn4_gallery-wrapper');
     let roof = gallery.offsetTop;
     let floor = roof + gallery.offsetHeight;
     let perimeter = window.innerHeight / 3;
     if (newPosition < roof - perimeter) {
-        document.querySelector('.sn5_nav').classList.remove('sn5_nav_fixed');
+        document.querySelector('.sn4_nav').classList.remove('sn4_nav_fixed');
     } else if (newPosition < floor - (perimeter * 2) && newPosition > roof - perimeter - 1) {
-        document.querySelector('.sn5_nav').classList.add('sn5_nav_fixed');
+        document.querySelector('.sn4_nav').classList.add('sn4_nav_fixed');
     }
 }
 
 function initBigGallery() {
-    let tilts = objectToArray(document.querySelectorAll('.sn5_container_gallery_img-positioner'));
+    let tilts = objectToArray(document.querySelectorAll('.sn4_container_gallery_img-positioner'));
     let IDletters = getBackgroundImageNames(tilts);
     tilts.forEach(function (tilt, idx) {
         tilt.addEventListener('click', function () {
@@ -79,7 +79,7 @@ function initBigGallery() {
 function getBackgroundImageNames(bgs) {
     let allPhotosIDLetters = [];
     bgs.forEach(function (bg) {
-        let bgURL = window.getComputedStyle(bg.querySelector('.sn5_gallery-image')).getPropertyValue('background-image');
+        let bgURL = window.getComputedStyle(bg.querySelector('.sn4_gallery-image')).getPropertyValue('background-image');
         let searchFor = (bgURL.indexOf('.jpg')) - 1;
         allPhotosIDLetters.push(bgURL.charAt(searchFor));
     });
@@ -90,9 +90,9 @@ function createBigGallery(ids, idx) {
     //console.log(thisPhoto, photoIdx);
     let id = ids[idx];
     let bigBG = document.createElement('div');
-    bigBG.classList.add('sn5_bigBG');
+    bigBG.classList.add('sn4_bigBG');
     let positioner = document.createElement('div');
-    positioner.classList.add('sn5_bigBG_positioner');
+    positioner.classList.add('sn4_bigBG_positioner');
     let IMG = document.createElement('img');
     IMG.setAttribute('src', './gallery/photo-' + id + '.jpg');
     bigBG.appendChild(positioner);
@@ -105,27 +105,27 @@ let bigGallerySwapCounter;
 function createBigGalleryNav(bg, idx, IMG) {
     bigGallerySwapCounter=idx;
     let nav = document.createElement('nav');
-    nav.classList.add('sn5_bigBG_nav');
+    nav.classList.add('sn4_bigBG_nav');
     bg.appendChild(nav);
     let a = document.createElement('div');
-    a.classList.add('sn5_bigBG_nav_positioner');
+    a.classList.add('sn4_bigBG_nav_positioner');
     nav.appendChild(a);
     let btns = [undefined, undefined, undefined];
     let icons = ['<i class="fas fa-arrow-left"></i>', '<i class="fas fa-times"></i>', '<i class="fas fa-arrow-right"></i >'];
     btns.forEach(function (btn, i) {
         btn = document.createElement('div');
-        btn.classList.add('sn5_bigBG_nav_positioned-btn');
+        btn.classList.add('sn4_bigBG_nav_positioned-btn');
         a.appendChild(btn);
         btn.innerHTML = icons[i];
-        let images = objectToArray(document.querySelectorAll('.sn5_container_gallery_img-positioner'));
+        let images = objectToArray(document.querySelectorAll('.sn4_container_gallery_img-positioner'));
         let idLetters = getBackgroundImageNames(images);
         if (i === 0) {
             letThisWork(btn, IMG, idLetters, (-1));
         } else if (i === 1) {
             btn.addEventListener('click', function () {
-                document.querySelector('.sn5_bigBG').classList.add('sn5_fade-out');
+                document.querySelector('.sn4_bigBG').classList.add('sn4_fade-out');
                 setTimeout(function(){
-                    document.body.removeChild(document.querySelector('.sn5_bigBG'));
+                    document.body.removeChild(document.querySelector('.sn4_bigBG'));
                 },300);
             });
         } else if (i === 2) {
@@ -141,8 +141,8 @@ function letThisWork(btn, img, ids, dir) {
         } else if (bigGallerySwapCounter<0){
             bigGallerySwapCounter = ids.length - 1
         };
-        img.classList.remove('sn5_fade-out-in');
-        setTimeout(function () {img.classList.add('sn5_fade-out-in')},0);
+        img.classList.remove('sn4_fade-out-in');
+        setTimeout(function () {img.classList.add('sn4_fade-out-in')},0);
         setTimeout(function () {
             img.setAttribute('src', './gallery/photo-' + ids[bigGallerySwapCounter] + '.jpg')
         }, 0);
